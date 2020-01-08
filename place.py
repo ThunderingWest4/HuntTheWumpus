f14eec17c64bbc74623a731ad1a00026b57b2b1d
ThunderingWest4 <ThunderingWest4@github.com>
2020-01-08 11:30:48 -0500

    i did a thing

--- place.py
+++ place.py
@@ -18,3 +18,27 @@ def genWumpus(start):
 
 numPit = 5
 numBat = 3
+
+class Environment():
+    #shape: 
+    #Pentagon where each point connects to a circle inside (5 points)
+    #Circle: 10 points, every other one connects to the outside pentagon
+    #the points that dont go to the outside pentagon go to the inner pentagon
+    #Inner Penta: 5 points
+    def __init__(self):
+        generated_points = []
+        outside_pentagon = []
+        decagon = []
+        inner_pentagon = []
+        #Generate outside Pentagon
+        for i in range(5):
+            found = False
+            while(found == False):
+                possible = random.randint(1, 20)
+                found = False if (possible in generated_points)
+
+            if(found == True):
+                outside_pentagon.append(possible)
+                generated_points.append(possible)
+        #Generate Decagon
+        
